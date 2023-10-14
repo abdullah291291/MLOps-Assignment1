753d37a3cd13f504bfea2f0457a885343feb7ff5
abdullah291291 <abdullahhameed291@gmail.com>
2023-10-15 03:47:14 +0500

    Changes in Features

--- model_train.py
+++ model_train.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
@@ -11,12 +10,6 @@ import pickle
 ## preparing dataset
 df = pd.read_csv("./data/data.csv")
 df = df[['price','bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','sqft_above','sqft_basement']]
-
-label_encoder = LabelEncoder()
-
-# Apply label encoding to the categorical column
-df['city'] = label_encoder.fit_transform(df['city'])
-df['statezip'] = label_encoder.fit_transform(df['statezip'])
 
 #splitting the Dataset
 X = df.drop(labels='price',axis=1)
